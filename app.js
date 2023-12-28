a65d49561b6f7574e01882f56ab143b74b3e72d3
Carlos Plata <Carlos.plata069@gmail.com>
2023-12-28 10:34:27 -0600

    Added If Comparer to state pusher and reordered the functions

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 let States = [];
 let Locations = [];
-
-fetchFile();
+locationsTransformer();
 
 async function fetchFile() {
   const requestURL =
-    "https://services2.arcgis.com/5I7u4SJE1vUr79JC/arcgis/rest/services/UniversityChapters_Public/FeatureServer/0/query?where=1%3D1&outFields=University_Chapter,City,State&outSR=4326&resultRecordCount=15&f=json";
+  "https://services2.arcgis.com/5I7u4SJE1vUr79JC/arcgis/rest/services/UniversityChapters_Public/FeatureServer/0/query?where=1%3D1&outFields=University_Chapter,City,State&outSR=4326&resultRecordCount=15&f=json";
   const request = new Request(requestURL);
 
   const response = await fetch(request);
-  const fileText = await response.text();
-
-  const data = JSON.parse(fileText);
-  locationsTransformer(data);
-
+  const data = await response.json();
+  
+  return (data);
 }
 
-function locationsTransformer(rawData) {
-
+function locationsTransformer() {
+  
+  rawData = fetchFile();
+  console.log(rawData);
   universities = rawData.features;
   universities.forEach(element => {
 
     let place = { location_name: element.attributes.University_Chapter, latLng: [element.geometry.x, element.geometry.y] };
 
-    States.push(element.attributes.State);
+    if (States.indexOf(element.attributes.State) === -1) {States.push(element.attributes.State)}
     Locations.push(place);
   });
 
