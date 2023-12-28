9d60b2a4d29b552f277fe87a61a4b50537ed7330
Carlos Plata <Carlos.plata069@gmail.com>
2023-12-28 10:37:39 -0600

    Brought fetchfile() down for easier readability

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ let States = [];
 let Locations = [];
 locationsTransformer();
 
+function locationsTransformer() {
+  
+  rawData = fetchFile();
+  console.log(rawData);
+  universities = rawData.features;
+  universities.forEach(element => {
+    
+    let place = { location_name: element.attributes.University_Chapter, latLng: [element.geometry.x, element.geometry.y] };
+    
+    if (States.indexOf(element.attributes.State) === -1) {States.push(element.attributes.State)}
+    Locations.push(place);
+  });
+  
+  console.log(States);
+  console.log(Locations);
+}
+
 async function fetchFile() {
   const requestURL =
   "https://services2.arcgis.com/5I7u4SJE1vUr79JC/arcgis/rest/services/UniversityChapters_Public/FeatureServer/0/query?where=1%3D1&outFields=University_Chapter,City,State&outSR=4326&resultRecordCount=15&f=json";
@@ -11,21 +28,4 @@ async function fetchFile() {
   const data = await response.json();
   
   return (data);
-}
-
-function locationsTransformer() {
-  
-  rawData = fetchFile();
-  console.log(rawData);
-  universities = rawData.features;
-  universities.forEach(element => {
-
-    let place = { location_name: element.attributes.University_Chapter, latLng: [element.geometry.x, element.geometry.y] };
-
-    if (States.indexOf(element.attributes.State) === -1) {States.push(element.attributes.State)}
-    Locations.push(place);
-  });
-
-  console.log(States);
-  console.log(Locations);
 }
